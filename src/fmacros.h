5b5d3cd94f4accb55ca7ddea819bb95eac56923c
Hideki ONO <onogit@yellowback.net>
2012-06-25 17:25:56 +0900

    Fixed for NetBSD

--- src/fmacros.h
+++ src/fmacros.h
@@ -2,8 +2,11 @@
 #define _REDIS_FMACRO_H
 
 #define _BSD_SOURCE
+#ifdef __NetBSD__
+#define _NETBSD_SOURCE
+#endif
 
-#if defined(__linux__) || defined(__OpenBSD__)
+#if defined(__linux__) || defined(__OpenBSD__) || defined(__NetBSD__)
 #define _XOPEN_SOURCE 700
 #else
 #define _XOPEN_SOURCE
